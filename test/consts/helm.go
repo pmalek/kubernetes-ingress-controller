74165c2f12458db941938724edc563aa0c26131d
Patryk Małek <patryk.malek@konghq.com>
2023-10-13 12:16:50 +0200

    feat(ci): add renovate config for kong helm chart

--- test/consts/helm.go
+++ test/consts/helm.go
@@ -1,4 +1,5 @@
 package consts
 
 // KongHelmChartVersion is the version of the Kong Helm chart to use in tests.
+// renovate: datasource=github-releases depName=helm/kong versioning=semver
 const KongHelmChartVersion = "2.26.3"
